c51cf1736a81262bd19ccdace9bc956d217c9d45
fadib <fahmi@fadib.net>
2014-03-27 00:01:26 -0400

    Loads Google Map API

--- wp-gmaps-shortcode.php
+++ wp-gmaps-shortcode.php
@@ -8,3 +8,15 @@ Author: Fahmi Adib
 Author URI: http://fadib.net
 Contributors: fahmiadib
 */
+
+
+/**
+ * Loads Google Map API
+ *
+ * @since       1.0
+ * @return      void
+*/
+function wp_gmaps_load_scripts() {
+	wp_register_script( 'wp-gmaps-api', '//maps.google.com/maps/api/js?sensor=false' );
+}
+add_action( 'wp_enqueue_scripts', 'wp_gmaps_load_scripts' );
