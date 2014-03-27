b7f1256094e8a6c55fb10e99f123690380176759
fadib <fahmi@fadib.net>
2014-03-26 23:54:05 -0400

    Add readme.txt and init file

---
+++ wp-gmaps-shortcode.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WP Google Maps Shortcode
+Plugin URL: http://fadib.net/wp-gmaps-shortcode
+Description: Adds a Google Maps short code within your post/page
+Version: 1.0
+Author: Fahmi Adib
+Author URI: http://fadib.net
+Contributors: fahmiadib
+*/
