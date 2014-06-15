3abfa239177357d480b074bede417ee2c35913f9
fadib <fahmi@fadib.net>
2014-06-14 23:33:09 -0400

    Release 1.1 : Utilize Transient API for cached maps

--- readme.txt
+++ readme.txt
@@ -4,8 +4,8 @@ Plugin URI: http://fadib.net/wp-google-maps-shortcode/
 Contributors: fahmiadib
 Tags: gmaps, google maps, short code, maps
 Requires at least: 3.5
-Tested up to: 3.9
-Stable tag: 1.0
+Tested up to: 3.9.1
+Stable tag: 1.1
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
@@ -30,6 +30,10 @@ Maps are displayed with the [wp_gmaps] short code:
 2. Add [wp_gmaps address="your address here"] or [wp_gmaps lat="your latitude" lng="your longitude"] to any post or page
 
 == Changelog ==
+
+= 1.1 =
+
+* Utilize Transients API for delivering cached maps
 
 = 1.0 =
 
--- wp-gmaps-shortcode.php
+++ wp-gmaps-shortcode.php
@@ -3,7 +3,7 @@
 Plugin Name: WP Google Maps Shortcode
 Plugin URL: http://fadib.net/wp-google-maps-shortcode
 Description: Insert Google Maps into your post or page using Shortcode
-Version: 1.0
+Version: 1.1
 Author: Fahmi Adib
 Author URI: http://fadib.net
 Contributors: fahmiadib
@@ -23,6 +23,14 @@ function wp_gmaps_shortcode( $atts ) {
 	), $atts );
 	
 	wp_print_scripts( 'wp-gmaps-api' );
+	
+	if ( $atts['address'] ) {
+		$coordinates = wp_gmaps_decode_address( $atts['address'] );
+		if ( is_array ( $coordinates ) ) {
+			$atts['lat'] = $coordinates['lat'];
+			$atts['lng'] = $coordinates['lng'];
+		}
+	}
 	
 	$map_id = uniqid( 'wp_gmaps_' );
 	
@@ -46,34 +54,19 @@ function wp_gmaps_shortcode( $atts ) {
 			map_<?php echo $map_id; ?> = new google.maps.Map(document.getElementById("<?php echo $map_id; ?>"), map_options);
 			
 			<?php if ( $atts['marker'] ): ?>
-			marker_<?php echo $map_id ?> = new google.maps.Marker({
-				position: location,
-				map: map_<?php echo $map_id; ?>
-			});
+				marker_<?php echo $map_id ?> = new google.maps.Marker({
+					position: location,
+					map: map_<?php echo $map_id; ?>
+				});
 			
 				<?php if ( $atts['infowindow'] ): ?>
-				infowindow_<?php echo $map_id; ?> = new google.maps.InfoWindow({
-					content: '<?php echo esc_attr( $atts['infowindow'] ) ?>'
-				});
-				google.maps.event.addListener(marker_<?php echo $map_id ?>, 'click', function() {
-					infowindow_<?php echo $map_id; ?>.open(map_<?php echo $map_id; ?>, marker_<?php echo $map_id ?>);
-				});
+					infowindow_<?php echo $map_id; ?> = new google.maps.InfoWindow({
+						content: '<?php echo esc_attr( $atts['infowindow'] ) ?>'
+					});
+					google.maps.event.addListener(marker_<?php echo $map_id ?>, 'click', function() {
+						infowindow_<?php echo $map_id; ?>.open(map_<?php echo $map_id; ?>, marker_<?php echo $map_id ?>);
+					});
 				<?php endif; ?>
-			
-			<?php endif; ?>
-			
-			<?php if ( $atts['address'] ): ?>
-				var address = "<?php echo esc_attr( $atts['address'] ) ?>";
-				geocoder.geocode( { 'address': address }, function(results, status) {
-					if (status == google.maps.GeocoderStatus.OK) {
-		  				map_<?php echo $map_id; ?>.setCenter(results[0].geometry.location);
-						if (marker_<?php echo $map_id ?>) {
-							marker_<?php echo $map_id ?>.setPosition(results[0].geometry.location);
-						}
-				    } else {
-				      	alert('Address not found: ' + address);
-				    }
-				});
 			<?php endif; ?>
 		}
 		wp_gmaps_<?php echo $map_id; ?>();
@@ -85,11 +78,66 @@ function wp_gmaps_shortcode( $atts ) {
 add_shortcode( 'wp_gmaps', 'wp_gmaps_shortcode' );
 
 /**
+ * Retrieve coordinates for an address
+ *
+ * Coordinates are cached using transients and a hash of the address
+ *
+ * @since       1.1
+ * @return      $array of latitude and longitude
+ */
+function wp_gmaps_decode_address( $address ) {
+    $address_hash = md5( $address );
+	
+    $coordinates = get_transient( $address_hash );
+	
+	if ( false === $coordinates ) {
+		$args = array( 'address' => urlencode( $address ) );
+		$url = add_query_arg( $args, 'http://maps.googleapis.com/maps/api/geocode/json' );
+     	$response = wp_remote_get( $url );
+		
+     	if ( is_wp_error( $response ) )
+     		return;
+
+		if ( $response['response']['code'] == 200 ) {
+	     	$data = wp_remote_retrieve_body( $response );
+			
+	     	if ( is_wp_error( $data ) )
+	     		return;
+			
+			$data = json_decode( $data );
+
+			if ( $data->status === 'OK' ) {
+			  	$coordinates = $data->results[0]->geometry->location;
+
+			  	$cache_value['lat'] = $coordinates->lat;
+			  	$cache_value['lng'] = $coordinates->lng;
+
+			  	// cache coordinates for 1 month
+			  	set_transient( $address_hash, $cache_value, 3600*24*30 );
+				$coordinates = $cache_value;
+
+			} elseif ( $data->status === 'ZERO_RESULTS' ) {
+			  	return __( 'No location found for the entered address.', 'wp-gmaps' );
+			} elseif( $data->status === 'INVALID_REQUEST' ) {
+			   	return __( 'Invalid request. Address is missing', 'wp-gmaps' );
+			} else {
+				return __( 'Something went wrong while retrieving your map.', 'wp-gmaps' );
+			}
+		} else {
+		 	return __( 'Unable to contact Google API service.', 'wp-gmaps' );
+		}
+		
+	}
+	
+	return $coordinates;
+}
+
+/**
  * Loads Google Map API
  *
  * @since       1.0
  * @return      void
-*/
+ */
 function wp_gmaps_load_scripts() {
 	wp_register_script( 'wp-gmaps-api', '//maps.google.com/maps/api/js?sensor=false' );
 }
